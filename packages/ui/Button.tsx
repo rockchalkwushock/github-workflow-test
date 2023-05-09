64941c83a6ad919ab5fc649369e571cad276c0f5
Cody Brunner <me@codybrunner.com>
2023-05-08 19:51:20 -0500

    feat: update button

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -3,5 +3,5 @@
 import * as React from 'react'
 
 export const Button = () => {
-  return <button onClick={() => alert('Beep')}>Beep 3</button>
+  return <button onClick={() => alert('Beep')}>Beep 4</button>
 }
